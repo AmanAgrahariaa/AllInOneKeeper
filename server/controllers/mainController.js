6c3fadae36c63d46c26e8dbc5381cf983a62d767
Aman Agrahari <104276710+AmanAgrahariaa@users.noreply.github.com>
2023-07-05 00:03:37 +0530

    checking..

---
+++ server/controllers/mainController.js
@@ -0,0 +1,19 @@
+exports.homepage = async (req,res)=>{
+     const locals = {
+          title: "NodeJs Notes",
+          description: "Notes App"
+     };
+     res.render('index',{
+          locals,
+          layout: '../views/layouts/front-page',
+     
+     }); 
+}
+
+exports.about = async (req,res)=>{
+     const locals = {
+          title: "NodeJs Notes",
+          description: "Notes App"
+     };
+     res.render('about',locals); 
+}
